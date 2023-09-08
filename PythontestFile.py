6cce22d34a4360070aee432f71a1c4f69ec17cdd
Wajahatali56 <140392808+Wajahatali56@users.noreply.github.com>
2023-09-08 22:40:53 +0500

    First commit

---
+++ PythontestFile.py
@@ -0,0 +1,44 @@
+
+from msilib.schema import Feature
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import streamlit as st
+
+
+data = pd.read_csv(r"C:\Users\USER PC\Desktop\Techproject\creditcard.csv")
+
+legit = data[data.Class == 0]
+fraud = data[data.Class == 1]
+
+legit_sample = legit.sample(n=len(fraud) , random_state=2)
+data = pd.concat([legit_sample , fraud] , axis=0)
+
+X = data.drop(columns="Class" , axis=1)
+Y = data["Class"]
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=2)
+
+model = LogisticRegression()
+model.fit(X_train , Y_train)
+
+train_acc = accuracy_score(model.predict(X_train) , Y_train)
+test_acc = accuracy_score(model.predict(X_test) , Y_test)
+
+
+# WeB App
+
+st.title("Credit Card Fraud Detection Model")
+input_df = st.text_input("Enter All Required Features Values")
+input_df_splited = input_df.split(',')
+
+submit = st.button("Submit")
+
+if submit:
+    features = np.asarray(input_df_splited , dtype=np.float64)
+    prediction = model.predict(features.reshape(1 , -1))
+    if prediction[0] == 0:
+        st.write("Legitimate Transaction")
+    else:
+        st.write("Fradulant Transaction")
